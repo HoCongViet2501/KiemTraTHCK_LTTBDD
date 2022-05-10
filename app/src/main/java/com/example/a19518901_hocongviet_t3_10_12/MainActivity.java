100b6666b64c4c017d4dea44d6c34f50fc41e073
unknown <viet.forwork.2501@gmail.com>
2022-05-10 15:15:27 +0700

    init

---
+++ app/src/main/java/com/example/a19518901_hocongviet_t3_10_12/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a19518901_hocongviet_t3_10_12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
